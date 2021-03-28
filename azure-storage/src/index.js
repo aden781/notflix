b314df5f99200e7bfb69ee0b64e7eb80de3d5792
M. Aden <aden781@gmail.com>
2021-03-28 15:28:15 -0500

    added db and video storage

--- azure-storage/src/index.js
+++ azure-storage/src/index.js
@@ -3,9 +3,23 @@ const azure = require('azure-storage')
 
 const app = express()
 
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.")
+}
+
+if (!process.env.STORAGE_ACCOUNT_NAME) {
+    throw new Error("Please specify the name of an Azure storage account in environment variable STORAGE_ACCOUNT_NAME.")
+}
+
+if (!process.env.STORAGE_ACCESS_KEY) {
+    throw new Error("Please specify the access key to an Azure storage account in environment variable STORAGE_ACCESS_KEY.")
+}
+
 const PORT = process.env.PORT
 const STORAGE_ACCOUNT_NAME = process.env.STORAGE_ACCOUNT_NAME
 const STORAGE_ACCESS_KEY = process.env.STORAGE_ACCESS_KEY
+
+console.log(`Serving videos from Azure storage account ${STORAGE_ACCOUNT_NAME}.`)
 
 const createBlobService = () => {
     const blobService = azure.createBlobService(STORAGE_ACCOUNT_NAME, STORAGE_ACCESS_KEY)
@@ -14,13 +28,16 @@ const createBlobService = () => {
 
 app.get('/video', (req, res) => {
     const videoPath = req.query.path
+    console.log(`Streaming video from path ${videoPath}.`)
+
     const blobService = createBlobService()
 
-    const containerName = 'videos'
+    const containerName = "videos"
     blobService.getBlobProperties(containerName, videoPath, (err, properties) => {
         if (err) {
-            console.log('An error occured')
-            res.send(500)
+            console.error(`Error occurred getting properties for video ${containerName}/${videoPath}.`);
+            console.error(err && err.stack || err);
+            res.sendStatus(500);
             return
         }
 
@@ -31,8 +48,8 @@ app.get('/video', (req, res) => {
 
         blobService.getBlobToStream(containerName, videoPath, res, err => {
             if (err) {
-                console.log('An error occured')
-                res.send(500)
+                console.log('An error occured there')
+                res.sendStatus(500)
                 return
             }
         })
